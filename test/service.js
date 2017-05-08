a67027e1cd8f3256c41edc35a5dfdf0c1ce96cb5
Maksym Radko <maksym.radko@swarmingtech.com>
2017-05-08 17:37:06 +0300

    done BUZ-131, add tests, add experimental Proxy validation

--- test/service.js
+++ test/service.js
@@ -1,11 +1,18 @@
 'use strict';
 
 const Promise = require('bluebird');
+const sinon = require('sinon');
+const path = require('path');
+const fs = require('fs');
 
 describe('Service', function () {
 
   const SDK = require('../');
-  const service = new SDK.Service();
+  const service = new SDK.Service(
+    '000000c1-1111-2222-3333-000000000000',
+    '1234567812345678123456781234567812345678123456781234567812345678',
+    'http://localhost:3000'
+  );
 
   it ('ping', function () {
     return service.ping();
@@ -44,4 +51,33 @@ describe('Service', function () {
     .then(response => service.remove(response.receipt));
   });
 
+  describe.only('uploadFiles', () => {
+    it('should be a function', () => {
+      expect(service.uploadFiles).to.be.a('function');
+    });
+
+    it('should send files when it is only one string', () => {
+      const stub = sinon.stub(service, 'request');
+      const fixturePath = path.join(__dirname, 'fixtures/files/hello.txt');
+      service.uploadFiles(fixturePath);
+      sinon.assert.calledOnce(stub);
+      stub.restore();
+    });
+
+    it('should send files when it is array of strings', () => {
+      const stub = sinon.stub(service, 'request');
+      const fixturePath1 = path.join(__dirname, 'fixtures/files/hello.txt');
+      const fixturePath2 = path.join(__dirname, 'fixtures/files/image.jpg');
+      service.uploadFiles([fixturePath1, fixturePath2]);
+      sinon.assert.calledOnce(stub);
+      stub.restore();
+    });
+
+    it('should send file and return correct result', async function() {
+      this.timeout(6000);
+      const fixturePath = path.join(__dirname, 'fixtures/files/hello.txt');
+      const result = await service.uploadFiles(fixturePath);
+      expect(JSON.parse(result)).to.be.an('object');
+    });
+  });
 });
